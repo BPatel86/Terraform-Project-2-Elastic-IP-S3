1f0431a93a7ce63b130930d9f7736ca5efeb1377
Binal Patel <breezy.binal@gmail.com>
2025-05-22 18:11:41 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAUJ3VT6ZC2435DRWP"
-    secret_key = "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"
+    access_key = "XXXXX"
+    secret_key = "XXXXX"
 }
